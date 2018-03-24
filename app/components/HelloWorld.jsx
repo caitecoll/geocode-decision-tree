0b8e6b473e22de3e3d74926531da5c335e379fec
kgresmer <kgresmer@gmail.com>
2018-03-24 16:19:44 -0500

    Displaying firebase data in the left nav.

--- app/components/HelloWorld.jsx
+++ app/components/HelloWorld.jsx
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import LeftNav from './LeftNav';
+import Grid from "react-bootstrap/es/Grid";
+import Row from "react-bootstrap/es/Row";
+import Col from "react-bootstrap/es/Col";
 
 const firebase = require('firebase');
 
 /* the main page for the index route of this app */
-const HelloWorld = () => {
-    var dbRef = firebase.app().database().ref('/categories/foodAndLiquidContainers');
-    var stages;
-    dbRef.on("value", function(snapshot) {
-        console.log(snapshot.val());
-    }, function (errorObject) {
-        console.log("The read failed: " + errorObject.code);
-    });
+class HelloWorld extends Component {
 
-    return (
-        <div>
-            <h1>Hello World!</h1>
-            <LeftNav/>
-        </div>
-    );
+    constructor() {
+        super();
+
+        this.state = {
+            stages: {}
+        }
+    }
+
+    componentDidMount() {
+        const dbRef = firebase.app().database().ref('/categories/foodAndLiquidContainers');
+        dbRef.on("value", (snapshot) => {
+            this.setState({stages: snapshot.val()});
+        }, function (errorObject) {
+            console.log("The read failed: " + errorObject.code);
+        });
+    }
+
+    render() {
+        return (
+            <Grid>
+                <Row className="show-grid">
+                    <Col xs={4} md={4} lg={4}>
+                        <LeftNav stages={this.state.stages}/>
+                    </Col>
+                    <Col xs={8} md={8} lg={8}>
+                        <h1>The Decision Tree</h1>
+                    </Col>
+                </Row>
+            </Grid>
+        );
+    }
+
 };
 
 export default HelloWorld;
--- app/components/LeftNav.jsx
+++ app/components/LeftNav.jsx
@@ -3,11 +3,21 @@ import ListGroup from 'react-bootstrap/lib/ListGroup';
 import ListGroupItem from 'react-bootstrap/lib/ListGroupItem';
 
 /* the main page for the index route of this app */
-const LeftNav = () => (
-  <ListGroup>
-    <ListGroupItem>Great Thing</ListGroupItem>
-    <ListGroupItem>Item 2</ListGroupItem>
-  </ListGroup>
-);
+const LeftNav = (stagesObject) => {
+    console.log(stagesObject.stages);
+    const displayStages = [];
+    for (let property in stagesObject.stages) {
+        if (stagesObject.stages.hasOwnProperty(property)) {
+            displayStages.push(stagesObject.stages[property]);
+        }
+    }
+    console.log(displayStages)
+    return (
+        <ListGroup>
+            <ListGroupItem>{displayStages[0] && displayStages[0].stage_name}</ListGroupItem>
+            <ListGroupItem>{displayStages[1] && displayStages[1].stage_name}</ListGroupItem>
+        </ListGroup>
+    );
+};
 
 export default LeftNav;
