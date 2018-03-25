dc7138774391936a230b2dee4d2f06b2cd2baa22
Caitlin Collins <caitecoll@gmail.com>
2018-03-25 11:40:36 -0500

    Adds styles to choices component

--- app/components/StageDisplay/StageDisplay.jsx
+++ app/components/StageDisplay/StageDisplay.jsx
@@ -1,29 +1,15 @@
 import React from 'react';
 import StageHeader from './components/StageHeader'
-import Choice from './components/Choice'
+import Choice from './components/Choice/Choice'
 import Grid from "react-bootstrap/es/Grid";
 import Row from "react-bootstrap/es/Row";
 
 const StageDisplay = (props) => {
   /* Need to change this to props.choices once we have that from the
   parent container, but just hard-coding in a constant for now */
-  const choices = [
-    {
-      title: "Test",
-      image: "https://www.fillmurray.com/200/300",
-      description: "awesome description"
-    },
-    {
-      title: "Test",
-      image: "https://www.fillmurray.com/200/300",
-      description: "awesome description"
-    },
-    {
-      title: "Test",
-      image: "https://www.fillmurray.com/200/300",
-      description: "awesome description"
-    },
-  ];
+  const choices = props.activeStage.choices || [];
+
+  console.log(props.activeStage);
 
   const stage = props.activeStage;
 
