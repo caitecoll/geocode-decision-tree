6d687ec75e43c005ed29b288ee7ec6c580a2a38e
kgresmer <kgresmer@gmail.com>
2018-03-24 21:55:41 -0500

    Removed style prop

--- app/components/HelloWorld.jsx
+++ app/components/HelloWorld.jsx
@@ -30,7 +30,7 @@ class HelloWorld extends Component {
         return (
             <Grid>
                 <Row className="show-grid">
-                    <Col xs={4} md={4} lg={4} style={}>
+                    <Col xs={4} md={4} lg={4} >
                         <LeftNav stages={this.state.stages}/>
                     </Col>
                     <Col xs={8} md={8} lg={8}>
